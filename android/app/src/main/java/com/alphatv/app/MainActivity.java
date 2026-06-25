852470b8215560aa16acf94870fd202a9126ebfa
ShahriarTbc <ripon23105101019@diu.edu.bd>
2026-06-25 08:08:46 +0600

    fix: change onDestroy access modifier to public in MainActivity.java

--- android/app/src/main/java/com/alphatv/app/MainActivity.java
+++ android/app/src/main/java/com/alphatv/app/MainActivity.java
@@ -86,7 +86,7 @@ public class MainActivity extends BridgeActivity {
     }
 
     @Override
-    protected void onDestroy() {
+    public void onDestroy() {
         super.onDestroy();
         try {
             unregisterReceiver(pipReceiver);
